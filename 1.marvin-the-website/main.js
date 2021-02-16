982e5b1d02ec1f9963048249f8c5929f5b9a07f4
Nuno <oleitao@sapo.pt>
2021-02-16 16:05:06 +0000

    first commit. to tidy up

---
+++ 1.marvin-the-website/main.js
@@ -0,0 +1,19 @@
+// function that displays alert and updates count on page
+function increaseCount() {
+  // display page alert
+  //alert("hehe");
+
+  // get the current count from the page
+  count = Number(document.getElementById("count").innerHTML);
+
+  // increase the count
+  count = count + 1;
+
+  // update the page
+  document.getElementById("count").innerHTML = count;
+}
+
+// attach the function to the button, so that it's triggered on each click
+document
+  .getElementsByTagName("button")[0]
+  .addEventListener("click", increaseCount);
